c8b51041b2922244aa586ce282667c7e130863ce
Andy Soffer <andy@brontosource.dev>
2025-04-08 21:46:52 -0400

    Add a very basic CMakeLists.txt.

--- bronto/bronto_compilation_test_source.cc
+++ bronto/bronto_compilation_test_source.cc
@@ -9,3 +9,5 @@ using Old BRONTO_INLINE() = New;
 #if BRONTO_REFACTOR
 #error This should never be executed because `BRONTO_REFACTOR` is not defined during compilation, only during the tool execution.
 #endif
+
+int main() { return 0; }
