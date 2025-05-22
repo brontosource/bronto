27fc76e0d6c6e00410929787e75b8b69bc620cb6
Andy Soffer <andy@brontosource.dev>
2025-05-22 15:25:12 -0400

    Add `BRONTO_BEFORE`, `BRONTO_AFTER` and `bronto::rewrite_expr` to the support library.

--- include/bronto/bronto.hpp
+++ include/bronto/bronto.hpp
@@ -3,11 +3,13 @@
 #ifndef BRONTO_BRONTO_H
 #define BRONTO_BRONTO_H
 
+#define BRONTO_INTERNAL_REQUIRE_NO_ARGS()
+
 // `BRONTO_INLINE()`:
 //
-// A function-like macro that can be applied to function definitions (free or member)
-// or to `using`-aliases to indicate that every usage of the
-// annotated entity should be replaced with its definition. For example,
+// A function-like macro that can be applied to function definitions (free or
+// member) or to `using`-aliases to indicate that every usage of the annotated
+// entity should be replaced with its definition. For example,
 //
 // ```
 // struct NewType {};
@@ -49,18 +51,45 @@
 // requiring the parentheses allows for configuration arguments to be added
 // without requiring a breaking change.
 //
-#define BRONTO_INTERNAL_REQUIRE_NO_ARGS()
 #define BRONTO_INLINE(...)                                                     \
   BRONTO_INTERNAL_REQUIRE_NO_ARGS(__VA_ARGS__)                                 \
   __attribute__((annotate("bronto::inline_me")))
+
+// `BRONTO_BEFORE()`:
+//
+// A function-like macro that can be applied to functions inside a
+// `bronto::rewrite_*` struct/class to indicate that the body of the function
+// represents some pattern to be matched in the code. See
+// https://brontosource.dev/docs for more details.
+#define BRONTO_BEFORE()                                                        \
+  BRONTO_INTERNAL_REQUIRE_NO_ARGS(__VA_ARGS__)                                 \
+  __attribute__((annotate("bronto::before")))
+
+// `BRONTO_AFTER()`:
+//
+// A function-like macro that can be applied to functions inside a
+// `bronto::rewrite_*` struct/class to indicate that the body of the function
+// represents some pattern to be used as a replacement in the code. See
+// https://brontosource.dev/docs for more details.
+#define BRONTO_AFTER()                                                         \
+  BRONTO_INTERNAL_REQUIRE_NO_ARGS(__VA_ARGS__)                                 \
+  __attribute__((annotate("bronto::after")))
+
+namespace bronto {
+
+/// `bronto::rewrite_expr`:
+///
+/// An empty struct used as a marker that any struct/class inheriting from it
+/// represents an expression replacement. Member functions can be annotated as
+/// `BRONTO_BEFORE` to indicate they represent a pattern to be searched for, or
+/// `BRONTO_AFTER` to indicate they represent a replacement pattern.
+struct rewrite_expr {};
+
+}  // namespace bronto
 
 // `BRONTO_REFACTOR`:
 //
 // A compilation flag which is left undefined in most builds, but which the
 // Bronto refactoring tool will specify as some nonzero integer literal.
-
-// Though currently empty, this namespace will contain additional utilities for
-// specifying refactorings.
-namespace bronto {}  // namespace bronto
 
 #endif  // BRONTO_BRONTO_H
